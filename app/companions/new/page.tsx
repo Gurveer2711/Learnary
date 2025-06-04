8cb8410c36dc9b6764e67887d9f9c7cace64a5c7
Gurveer2711 <singhgurveer337@gmail.com>
2025-06-04 18:40:13 +0530

    Created all the necessary folders and pages for the app

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanionBuilder = () => {
+  return (
+    <div>CompanionBuilder</div>
+  )
+}
+
+export default CompanionBuilder
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl underline'>Welcome to my app</h1>
+      <Button className=''>
+        Lets get Started
+      </Button>
+    </div>
   )
 }
 
