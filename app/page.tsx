02b1a07278c7ca11652da12df22b0660e89fd9a6
Gurveer2711 <singhgurveer337@gmail.com>
2025-06-05 22:24:27 +0530

    enhance homepage structure with CompanionCard, CompanionList, and CTA components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
+import CompanionCard from '@/components/CompanionCard'
 import { Button } from '@/components/ui/button'
 import React from 'react'
-
+import CompanionList from '@/components/CompanionList'
+import Cta from '@/components/Cta'
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my app</h1>
-      <Button className=''>
-        Lets get Started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          subject="Science"
+          name="Neura the brainy explorer"
+          topic="Neural network of the brain"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="123"
+          subject="Maths"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={30}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="123"
+          subject="Language"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionList />
+        <Cta />
+      </section>
+    </main>
   )
 }
 
