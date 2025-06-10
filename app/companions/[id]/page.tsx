e961ab5f3ecdb2de3b4d66afad20d6760bedef07
Gurveer27 <singhgurveer337@gmail.com>
2025-06-10 20:27:44 +0530

    Add CompanionComponent for interactive session management and integrate voice assistant functionality

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 interface CompanionSessionPageProps{
   params: Promise<{ id: string }>
 }
@@ -12,25 +13,25 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
   const companion = await getCompanion(id);
   const user = await currentUser();
-
+  const {name, subject, topic, duration } = companion;
   if (!user) {
     redirect('/sign-in');
   }
   if (!companion) {
     redirect('/companions');
   }
-  console.log(companion);
+  
   return (
     <main>
-      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+      <article className="flex rounded-md border-black border-2 justify-between p-6 max-md:flex-col">
         <div className="flex items-center gap-2">
           <div
-            className="size-[72px] flex items-center justify-center rounded-lg"
-            style={{backgroundColor:getSubjectColor(companion.subject)}}
+            className="size-[72px] flex items-center justify-center rounded-md"
+            style={{backgroundColor:getSubjectColor(subject)}}
           >
             <Image
-              src={`/icons/${companion.subject}.svg`}
-              alt={companion.subject}
+              src={`/icons/${subject}.svg`}
+              alt={subject}
               width={35}
               height={35}
             />
@@ -38,16 +39,22 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           
           <div className="flex flex-row gap-2">
             <div className="flex flex-col items-left gap-2">
-              <p className="font-bold text-2xl">{companion.name}</p>
-              <p className="text-md">Topic: {companion.topic}</p>
+              <p className="font-bold text-2xl">{name}</p>
+              <p className="text-md">Topic: {topic}</p>
             </div>
-            <div className="subject-badge max-h-7">{companion.subject}</div>
+            <div className="subject-badge max-h-7">{subject}</div>
           </div>
         </div>
-        <div className="text-md"><span className="font-semibold">Duration: </span>{companion.duration} Mins</div>
+        <div className="text-md"><span className="font-semibold">Duration: </span>{duration} Mins</div>
       </article>
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName}
+        userImage={user.imageUrl}
+      />
     </main>
   )
 }
 
-export default CompanionSession
+export default CompanionSession;
